88a37c2e6fd1531db978da8682a72a71c8fd5d3e
Joe Cooper <swelljoe@gmail.com>
2017-09-10 18:48:34 -0500

    Start working on making unicode work, maybe

--- sh/make-image.sh
+++ sh/make-image.sh
@@ -4,14 +4,15 @@ author=$2
 date=$(date +%D -d "$3")
 ferritcode=$4
 
+echo "$@" >> make-image.log
 mkdir -p tmp/
 mkdir -p public/img
 
 # Overlay message
-gm convert -font "ttf/PTN77F.ttf" -pointsize 98 -fill '#232323' -draw "text 289,393 '“${message}”\n— ${author}'" assets/img/ferrit-bg-blank.png "tmp/${ferritcode}.png"
+gm convert -encoding Unicode -font "ttf/PTN77F.ttf" -pointsize 98 -fill '#232323' -draw "text 289,393 '“${message}”\n— ${author}'" assets/img/ferrit-bg-blank.png "tmp/${ferritcode}.png"
 
 # Overlay ferrit code
-gm convert -font "ttf/PTN77F.ttf" -pointsize 48 -fill '#232323' -draw "text 900,953 '${ferritcode}'" "tmp/${ferritcode}.png" "tmp/${ferritcode}.png"
+gm convert -encoding Unicode -font "ttf/PTN77F.ttf" -pointsize 48 -fill '#232323' -draw "text 900,953 '${ferritcode}'" "tmp/${ferritcode}.png" "tmp/${ferritcode}.png"
 
 # Overlay QR code
 gm composite -geometry +1896+840 "tmp/qr-${ferritcode}.png" "tmp/${ferritcode}.png" "tmp/${ferritcode}.png"
@@ -20,8 +21,8 @@ gm composite -geometry +1896+840 "tmp/qr-${ferritcode}.png" "tmp/${ferritcode}.p
 gm convert -rotate 90 "tmp/${ferritcode}.png" "tmp/${ferritcode}.png"
 
 # Source
-gm convert -font "ttf/PTN77F.ttf" -pointsize 30 -fill '#232323' -draw "text 111,100 'Source'" "tmp/${ferritcode}.png" "tmp/${ferritcode}.png"
-gm convert -font "ttf/PTN77F.ttf" -pointsize 42 -fill '#232323' -draw "text 111,140 'Twitter ${date}'" "tmp/${ferritcode}.png" "tmp/${ferritcode}.png"
+gm convert -encoding Unicode -font "ttf/PTN77F.ttf" -pointsize 30 -fill '#232323' -draw "text 111,100 'Source'" "tmp/${ferritcode}.png" "tmp/${ferritcode}.png"
+gm convert -encoding Unicode -font "ttf/PTN77F.ttf" -pointsize 42 -fill '#232323' -draw "text 111,140 'Twitter ${date}'" "tmp/${ferritcode}.png" "tmp/${ferritcode}.png"
 
 # Rotate it back
 gm convert -rotate '-90' "tmp/${ferritcode}.png" "tmp/${ferritcode}.png"
